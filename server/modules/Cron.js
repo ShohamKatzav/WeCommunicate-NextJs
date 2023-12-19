ef47f62bd9d9b9d438fae5827410c023b85520f7
Shoham Katzav <shohamkatzav95@gmail.com>
2023-12-19 04:24:37 +0200

    Modified cron settings to exclude execution during idle hours (00:00-08:00 am)

--- server/modules/Cron.js
+++ server/modules/Cron.js
@@ -2,7 +2,7 @@
 const cron = require('node-cron');
 
 const myCronJob = () => {
-    cron.schedule('*/14 * * * *', () => {
+    cron.schedule('*/14 8-23 * * *', () => {
         fetch(`${process.env.BASE_URL}/api/v1/wake-up`)
             .then(res => res.json())
             .then(data => console.log(data))
--- server/routes/WakeUp.js
+++ server/routes/WakeUp.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const Test = async (req, res) => {
-    console.log("Waking up...");
     res.status(200).json({ message: "Successfully woke up" });
 }
 
