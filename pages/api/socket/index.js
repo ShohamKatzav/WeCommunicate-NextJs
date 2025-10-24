76cf82b365453d12e137d2304fad077de8ac917a
Shoham Katzav <shohamkatzav95@gmail.com>
2025-10-24 18:54:01 +0300

    RedisServis moved out of api folder

--- pages/api/socket/index.js
+++ pages/api/socket/index.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 import proxy from "../../../proxy"
 import { GetLocations, SaveLocations } from "@/app/api/location/locations";
 import Conversation from '@/app/api/models/Conversation'
-import RedisService from "../../RedisService";
+import RedisService from '@/services/RedisService'
 
 let redis = null;
 let io = null;
