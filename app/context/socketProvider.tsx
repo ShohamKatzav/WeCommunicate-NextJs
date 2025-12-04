67b808f3f9c2d11ad94f6c65ad675ae75ee30104
unknown <shohamkatzav95@gmail.com>
2025-12-05 01:19:20 +0200

    removed websocket from transports

--- app/context/socketProvider.tsx
+++ app/context/socketProvider.tsx
@@ -29,7 +29,6 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
                     reconnectionDelay: 10000,
                     reconnectionDelayMax: 10000,
                     path: "/api/socket",
-                    transports: ['websocket', 'polling'],
                     auth: {
                         token: user?.token
                     }
